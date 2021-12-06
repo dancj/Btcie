2036aa5783010aa0101937c5bfed6d5cdda36b63
Dan <dan@fairedev.com>
2021-12-06 00:37:42 -0500

    add some more routing, which seems to work

--- web/tracker/urls.py
+++ web/tracker/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /tracker/
     path('', views.index, name='index'),
+
+    # /tracker/5
+    path('<int:tx_id>/', views.detail, name='detail'),
+
+    # /tracker/portfolio
+    path('portfolio/', views.portfolio, name='portfolio')
 ]
--- web/tracker/views.py
+++ web/tracker/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the trackerp index.")
+    return HttpResponse("This is an awesome portfolio history tracker");
+
+def detail(request, tx_id):
+    return HttpResponse("You're looking at transaction id %s." % tx_id)
+
+def portfolio(request):
+    return HttpResponse("This is the current portfolio state");
